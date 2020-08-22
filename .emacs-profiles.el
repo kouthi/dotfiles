5dd61ba04a9018e931119b0bd1a852aa5a4cd33a
kouthi <kouthi@users.noreply.github.com>
2020-08-22 16:43:35 +0900

    Change spacemacs install directory under Chemacs

--- .emacs-profiles.el
+++ .emacs-profiles.el
@@ -1,3 +1,3 @@
 (("default" . ((user-emacs-directory . "~/.emacs.d")))
-  ("doom" . ((user-emacs-directory . "~/.doom-emacs")))
-  ("spacemacs" . ((user-emacs-directory . "~/.spacemacs"))))
+ ("doom" . ((user-emacs-directory . "~/.doom-emacs")))
+ ("spacemacs" . ((user-emacs-directory . "~/.spacemacs.install"))))
