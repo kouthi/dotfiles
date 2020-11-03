6a4b18c1751892bbcbc3d1c481a69304e70fb45b
kouthi <kouthi@users.noreply.github.com>
2020-11-04 06:58:52 +0900

    Boyscouting

--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -10,14 +10,14 @@
 ;(package! some-package)
 (package! howm)
 (package! pangu-spacing :disable t)
-(package! valign
-          :recipe (:host github :repo "casouri/valign"))
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/raxod502/straight.el#the-recipe-format
 ;(package! another-package
 ;  :recipe (:host github :repo "username/repo"))
+(package! valign
+  :recipe (:host github :repo "casouri/valign"))
 
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
