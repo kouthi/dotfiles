3f079258d7b3cd335224685dcfe2c18625d11bd1
kouthi <kouthi@users.noreply.github.com>
2022-05-22 16:43:11 +0900

    Update default init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,13 +3,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (leuven)))
+ '(custom-enabled-themes '(leuven))
  '(inhibit-startup-screen t)
  '(make-backup-files nil)
- '(setq leuven-scale-outline-headlines nil))
+ '(setq leuven-scale-outline-headlines))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "HackGenNerd Console" :foundry "PfEd" :slant normal :weight normal :height 241 :width normal)))))
