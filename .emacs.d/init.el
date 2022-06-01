799f41798f7771da0fd59a59242b1c986ec65e4d
kouthi <kouthi@users.noreply.github.com>
2022-06-01 15:03:26 +0900

    Change default Emacs font

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -12,4 +12,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "HackGenNerd Console" :foundry "PfEd" :slant normal :weight normal :height 241 :width normal)))))
+ (if (string-match "IO" (system-name))
+     '(default ((t (:family "HackGenNerd Console" :foundry "PfEd" :slant normal :weight normal :height 241 :width normal))))
+   '(default ((t (:family "HackGenNerd Console" :foundry "PfEd" :slant normal :weight normal :height 140 :width normal))))))
