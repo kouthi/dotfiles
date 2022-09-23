fcd802c9070a1bfd4b3c1fb3436562349146632b
kouthi <kouthi@users.noreply.github.com>
2022-09-23 17:50:31 +0900

    Set leuve-theme's headline height in default emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,10 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(leuven-scale-outline-headlines nil)
  '(custom-enabled-themes '(leuven))
  '(inhibit-startup-screen t)
- '(make-backup-files nil)
- '(setq leuven-scale-outline-headlines))
+ '(make-backup-files nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
