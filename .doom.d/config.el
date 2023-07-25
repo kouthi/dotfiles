54ebe6145100fa314392a29dd4d5e5ecc9a2e1b5
kouthi <kouthi@users.noreply.github.com>
2023-07-25 16:18:43 +0900

    Change font size settings for doom-emacs

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -22,10 +22,10 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "HackGen Console NF" :size 18)
+(setq doom-font (font-spec :8amily "HackGen Console NF" :size 14)
       doom-variable-pitch-font (font-spec :family "HackGen35 Console NF")
       doom-unicode-font (font-spec :family "HackGen Console NF")
-      doom-big-font (font-spec :family "HackGen Console NF" :size 27))
+      doom-big-font (font-spec :family "HackGen Console NF" :size 21))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
